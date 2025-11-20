fec40e786566ef213b97bbcea4f63c1b5b82d7d9
Lucca Pellegrini <lucca@verticordia.com>
2025-11-20 17:07:23 -0300

    chore(app): bump flutter version

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.verticordia.lockwise"
+    namespace = "com.example.fechaduraflow"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.verticordia.lockwise"
+        applicationId = "com.example.fechaduraflow"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- app/android/app/src/main/kotlin/com/example/fechaduraflow/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/fechaduraflow/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.verticordia.lockwise
+package com.example.fechaduraflow
 
 import io.flutter.embedding.android.FlutterActivity
 
